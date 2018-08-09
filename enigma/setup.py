a715d499415c57c8c60870260652219a6c8d6e0b
Andre Filliettaz <andrentaz@gmail.com>
2018-08-09 19:29:21 -0300

    Setup application as a module

---
+++ enigma/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="authentication",
+    version="0.0.1",
+    author="Andre Filliettaz",
+    author_email="andrentaz@gmail.com",
+    description="A small authentication server",
+    url="https://github.com/andrentaz/caucious-octo-enigma.git",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        "flask",
+        "flask-cors",
+    ]
+)
